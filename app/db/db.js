69271c20f63ecae549a5137fcad2cfa1f552ece7
Lucas Steffens <lucassteffensoliveira@hotmail.com>
2024-09-01 15:27:38 -0400

    comit

---
+++ app/db/db.js
@@ -0,0 +1,17 @@
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./contracts.db'); // Inicializa o banco de dados de contrato
+
+db.serialize(() => {
+    db.run(`CREATE TABLE IF NOT EXISTS contracts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    cnpj TEXT,
+    razaoSocial TEXT,
+    dataVigenciaInicio TEXT,
+    dataVigenciaFim TEXT,
+    nomeRazaoSocialFornecedor TEXT,
+    objetoContrato TEXT,
+    valorInicial REAL
+  )`);
+});
+
+module.exports = db;
