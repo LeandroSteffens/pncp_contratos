4ab0d33ea3b360d9afd7966b966244826008e521
Leandro Steffens de Oliveira <84768349+LeandroSteffens@users.noreply.github.com>
2024-09-02 18:24:27 -0400

    utils add function and simuleResponse is data object

--- app/routes/searchContracts.js
+++ app/routes/searchContracts.js
@@ -9,6 +9,13 @@ const { dateToString } = require("../utils/utils");
 
 // Função para inserir contratos no banco de dados
 const insertContractsIntoDB = (contracts) => {
+  if (!Array.isArray(contracts)) {
+    console.error(
+      "Expected contracts to be an array, but got:",
+      typeof contracts
+    );
+    return;
+  }
   contracts.forEach((contract) => {
     db.run(
       `INSERT INTO contracts (cnpj, razaoSocial, dataVigenciaInicio, dataVigenciaFim, nomeRazaoSocialFornecedor, objetoContrato, valorInicial) VALUES (?, ?, ?, ?, ?, ?, ?)`,
@@ -30,12 +37,12 @@ const calculateTotalValue = (contracts) => {
   return contracts.reduce((sum, contract) => sum + contract.valorInicial, 0);
 };
 
-const makeRequest = async (url, mockResponse) => {
+const makeRequest = async (url) => {
   let response;
   try {
     response = await axios.get(url, { timeout: 5000 });
   } catch (error) {
-    response = mockResponse;
+    response = simuleResponse;
   }
   return response;
 };
@@ -48,7 +55,7 @@ router.get("/buscarContratos", async (req, res, next) => {
   )}&dataFinal=${dateToString(dataFinal)}&cnpjOrgao=${cnpj}&pagina=1`;
 
   try {
-    const response = await makeRequest(url, simuleResponse); // Realiza a request para o endpoint da API, simula o resultado em caso de falha
+    const response = await makeRequest(url); // Realiza a request para o endpoint da API, simula o resultado em caso de falha
     const contracts = response.data.data;
 
     insertContractsIntoDB(contracts); // Chama funcao que realiza a inserção dos contratos no banco de dados
@@ -67,79 +74,81 @@ router.get("/buscarContratos", async (req, res, next) => {
 
 // Simule a resposta caso a API não esteja disponível ou devolva algum erro não esperado
 const simuleResponse = {
-  data: Array.from({ length: 10 }, (_, i) => ({
-    numeroControlePncpCompra: `12345${i}`,
-    codigoPaisFornecedor: "BR",
-    orgaoSubRogado: {
-      cnpj: `1234567800010${i}`,
-      razaoSocial: `Orgao SubRogado ${i}`,
-      poderId: "1",
-      esferaId: "1",
-    },
-    orgaoEntidade: {
-      cnpj: `1234567800010${i + 1}`,
-      razaoSocial: `Orgao Entidade ${i}`,
-      poderId: "1",
-      esferaId: "1",
-    },
-    anoContrato: 2023,
-    tipoContrato: {
-      id: 1,
-      nome: `Tipo ${i}`,
-    },
-    numeroContratoEmpenho: `123456${i}`,
-    dataAssinatura: "2024-09-01",
-    dataVigenciaInicio: "2024-09-01",
-    dataVigenciaFim: "2024-09-01",
-    niFornecedor: `12345678${i}`,
-    tipoPessoa: "PJ",
-    categoriaProcesso: {
-      id: 1,
-      nome: `Categoria ${i}`,
-    },
-    dataPublicacaoPncp: "2024-08-31T10:44:08",
-    dataAtualizacao: "2024-08-31T10:44:08",
-    sequencialContrato: i + 1,
-    unidadeOrgao: {
-      ufNome: "São Paulo",
-      codigoUnidade: `SP0${i}`,
-      nomeUnidade: `Unidade ${i}`,
-      ufSigla: "SP",
-      municipioNome: "São Paulo",
-      codigoIbge: "3550308",
-    },
-    informacaoComplementar: `Informação complementar ${i}`,
-    processo: `Processo ${i}`,
-    unidadeSubRogada: {
-      ufNome: "Rio de Janeiro",
-      codigoUnidade: `RJ0${i}`,
-      nomeUnidade: `Unidade ${i + 1}`,
-      ufSigla: "RJ",
-      municipioNome: "Rio de Janeiro",
-      codigoIbge: "3304557",
-    },
-    nomeRazaoSocialFornecedor: `Fornecedor ${i}`,
-    niFornecedorSubContratado: `87654321${i}`,
-    nomeFornecedorSubContratado: `Fornecedor Subcontratado ${i}`,
-    numeroControlePNCP: `54321${i}`,
-    receita: true,
-    tipoPessoaSubContratada: "PJ",
-    objetoContrato: `Objeto do contrato ${i}`,
-    valorInicial: 1000 + i * 100,
-    numeroParcelas: 10,
-    valorParcela: 100,
-    valorGlobal: 1000 + i * 100,
-    valorAcumulado: 1000 + i * 100,
-    numeroRetificacao: 0,
-    identificadorCipi: `CIPI12345${i}`,
-    urlCipi: `http://example.com/cipi12345${i}`,
-    usuarioNome: `Usuário ${i}`,
-  })),
-  totalRegistros: 10,
-  totalPaginas: 1,
-  numeroPagina: 1,
-  paginasRestantes: 0,
-  empty: false,
+  data: {
+    data: Array.from({ length: 10 }, (_, i) => ({
+      numeroControlePncpCompra: `12345${i}`,
+      codigoPaisFornecedor: "BR",
+      orgaoSubRogado: {
+        cnpj: `1234567800010${i}`,
+        razaoSocial: `Orgao SubRogado ${i}`,
+        poderId: "1",
+        esferaId: "1",
+      },
+      orgaoEntidade: {
+        cnpj: `1234567800010${i + 1}`,
+        razaoSocial: `Orgao Entidade ${i}`,
+        poderId: "1",
+        esferaId: "1",
+      },
+      anoContrato: 2023,
+      tipoContrato: {
+        id: 1,
+        nome: `Tipo ${i}`,
+      },
+      numeroContratoEmpenho: `123456${i}`,
+      dataAssinatura: "2024-09-01",
+      dataVigenciaInicio: "2024-09-01",
+      dataVigenciaFim: "2024-09-01",
+      niFornecedor: `12345678${i}`,
+      tipoPessoa: "PJ",
+      categoriaProcesso: {
+        id: 1,
+        nome: `Categoria ${i}`,
+      },
+      dataPublicacaoPncp: "2024-08-31T10:44:08",
+      dataAtualizacao: "2024-08-31T10:44:08",
+      sequencialContrato: i + 1,
+      unidadeOrgao: {
+        ufNome: "São Paulo",
+        codigoUnidade: `SP0${i}`,
+        nomeUnidade: `Unidade ${i}`,
+        ufSigla: "SP",
+        municipioNome: "São Paulo",
+        codigoIbge: "3550308",
+      },
+      informacaoComplementar: `Informação complementar ${i}`,
+      processo: `Processo ${i}`,
+      unidadeSubRogada: {
+        ufNome: "Rio de Janeiro",
+        codigoUnidade: `RJ0${i}`,
+        nomeUnidade: `Unidade ${i + 1}`,
+        ufSigla: "RJ",
+        municipioNome: "Rio de Janeiro",
+        codigoIbge: "3304557",
+      },
+      nomeRazaoSocialFornecedor: `Fornecedor ${i}`,
+      niFornecedorSubContratado: `87654321${i}`,
+      nomeFornecedorSubContratado: `Fornecedor Subcontratado ${i}`,
+      numeroControlePNCP: `54321${i}`,
+      receita: true,
+      tipoPessoaSubContratada: "PJ",
+      objetoContrato: `Objeto do contrato ${i}`,
+      valorInicial: 1000 + i * 100,
+      numeroParcelas: 10,
+      valorParcela: 100,
+      valorGlobal: 1000 + i * 100,
+      valorAcumulado: 1000 + i * 100,
+      numeroRetificacao: 0,
+      identificadorCipi: `CIPI12345${i}`,
+      urlCipi: `http://example.com/cipi12345${i}`,
+      usuarioNome: `Usuário ${i}`,
+    })),
+    totalRegistros: 10,
+    totalPaginas: 1,
+    numeroPagina: 1,
+    paginasRestantes: 0,
+    empty: false,
+  },
 };
 
 module.exports = router;
--- app/utils/utils.js
+++ app/utils/utils.js
@@ -1,7 +1,16 @@
-
-// Arquivo  responsavel por conter funções uteis para o sistema, como conversões e manipulações de dados
+// Arquivo responsavel por conter funções uteis para o sistema, como conversões e manipulações de dados
 function dateToString(dateString) {
-    return dateString.replace(/-/g, '');
+  if (!dateString) {
+    return "";
+  }
+  return dateString.replace(/-/g, "");
 }
 
-module.exports = {dateToString};
+function removeCnpjMask(cnpj) {
+  if (!cnpj) {
+    return "";
+  }
+  return cnpj.replace(/\D/g, "");
+}
+
+module.exports = { dateToString, removeCnpjMask };
